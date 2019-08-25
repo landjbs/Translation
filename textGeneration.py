d6b68c602dd76ff937350c9761abf555ec858774
Landon Smith <landjbs@gmail.com>
2019-08-25 17:12:22 -0400

    vectorize_context_seq init

--- textGeneration.py
+++ textGeneration.py
@@ -6,13 +6,20 @@ from bert_serving.client import BertClient
 
 SEQ_SIZE = 100
 BERT_PATH = '/Users/landonsmith/Desktop/shortBert'
+TEXT_PATH = 'gatsby.txt'
+
 
 # configure BERT client
 bc = BertClient(check_length=True)
 bertLaunch = f'bert-serving-start -model_dir {BERT_PATH} -num_worker=3 -max_seq_len={SEQ_SIZE} -pooling-strategy=NONE'
 # bert-serving-start -model_dir /Users/landonsmith/Desktop/shortBert -num_worker=3 -max_seq_len=100 -pooling-strategy=NONE
 
+
 def text_to_word_ids(textPath):
+    """
+    Builds list of word ids for each word in text, encoded with wordIdx and
+    decipherable with reverseIdx
+    """
     lenList = []
     wordIdx = dict()
     with open(textPath, 'r') as textFile:
@@ -25,5 +32,10 @@ def text_to_word_ids(textPath):
         textIds = list(map(word_to_id, textWords))
     return textIds, wordIdx, reverseIdx
 
+textIds, wordIdx, reverseIdx = text_to_word_ids()
 
-def encode
+def vectorize_context_seq(idList, reverseIdx):
+    seqString = ' '.join(reverseIdx[wordId] for wordId in idList)
+    print(seqString)
+
+vectorize_context_seq(['hi', 'how', 'are', 'you'], reverseIdx)
