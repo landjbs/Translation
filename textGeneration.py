b1471342e6e40cc9a95d28ef2fa50bc9bd34f741
Landon Smith <landjbs@gmail.com>
2019-08-26 11:17:24 -0400

    vectorize_context targets

--- textGeneration.py
+++ textGeneration.py
@@ -11,8 +11,8 @@ TEXT_PATH = 'gatsby.txt'
 
 # configure BERT client
 bc = BertClient(check_length=True)
-bertLaunch = f'bert-serving-start -model_dir {BERT_PATH} -num_worker=3 -max_seq_len={SEQ_SIZE} -pooling-strategy=NONE'
-# bert-serving-start -model_dir /Users/landonsmith/Desktop/shortBert -num_worker=3 -max_seq_len=100 -pooling-strategy=NONE
+bertLaunch = f'bert-serving-start -model_dir {BERT_PATH} -num_worker=3 -max_seq_len={SEQ_SIZE + 2} -pooling_strategy=NONE'
+# bert-serving-start -model_dir /Users/landonsmith/Desktop/shortBert -num_worker=3 -max_seq_len=102 -pooling_strategy=NONE
 
 
 def text_to_word_ids(textPath):
@@ -26,16 +26,27 @@ def text_to_word_ids(textPath):
         rawText = textFile.read()
         cleanText = rawText.lower()
         textWords = word_tokenize(cleanText, language='english')
-        wordIdx = {word : i for i, word in enumerate(textWords)}
-        reverseIdx = {i : word for word, i in wordIdx.items()}
+        wordIdx = {word : i for i, word in tqdm(enumerate(textWords))}
+        reverseIdx = {i : word for word, i in tqdm(wordIdx.items())}
         word_to_id = lambda word : wordIdx[word]
-        textIds = list(map(word_to_id, textWords))
+        textIds = [word_to_id(word) for word in tqdm(textWords)]
     return textIds, wordIdx, reverseIdx
 
-textIds, wordIdx, reverseIdx = text_to_word_ids()
+
+textIds, wordIdx, reverseIdx = text_to_word_ids(TEXT_PATH)
+
 
 def vectorize_context_seq(idList, reverseIdx):
-    seqString = ' '.join(reverseIdx[wordId] for wordId in idList)
-    print(seqString)
+    """
+    Vectorizes first seqSize tokens of idList using contextual attention
+    and outputs feature matrix of token embeddings and target word id
+    """
+    assert (len(idList)==(SEQ_SIZE + 1))
+    seqWords = [reverseIdx[wordId] for wordId in idList[:-1]]
+    vectorMatrix = bc.encode([seqWords], is_tokenized=True)[0]
+    targetId = idList[-1]
+    return vectorMatrix, targetId
 
-vectorize_context_seq(['hi', 'how', 'are', 'you'], reverseIdx)
+for i in range(0, 1000):
+    _, x = vectorize_context_seq(textIds[i:(i+101)], reverseIdx)
+    print(reverseIdx[x])
