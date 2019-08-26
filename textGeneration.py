89fd9a775f13bdf08c4517ccb1a492a098db51ed
Landon Smith <landjbs@gmail.com>
2019-08-26 12:15:58 -0400

    fit generator

--- textGeneration.py
+++ textGeneration.py
@@ -30,11 +30,8 @@ def text_to_word_ids(textPath):
         reverseIdx = {i : word for word, i in tqdm(wordIdx.items())}
         word_to_id = lambda word : wordIdx[word]
         textIds = [word_to_id(word) for word in tqdm(textWords)]
-        vocabSize = len(wordIdx)
+        vocabSize = max(reverseIdx)
     return textIds, wordIdx, reverseIdx, vocabSize
-
-
-textIds, wordIdx, reverseIdx, vocabSize = text_to_word_ids(TEXT_PATH)
 
 
 def one_hot_encode(wordId, vocabSize):
@@ -49,14 +46,42 @@ def vectorize_context_seq(idList, reverseIdx, vocabSize):
     and outputs feature matrix of token embeddings and one hot vector encoding
     target word id
     """
-    assert (len(idList) == (SEQ_SIZE + 1))
+    assert (len(idList) == (SEQ_SIZE + 1)), f'Invalid idList length of {len(idList)}'
     seqWords = [reverseIdx[wordId] for wordId in idList[:-1]]
     vectorMatrix = bc.encode([seqWords], is_tokenized=True)[0]
+    # remove cls and start
+    filteredMatrix = vectorMatrix[1:-1]
+    assert (len(filteredMatrix) == SEQ_SIZE), f'Invalid vector matrix shape of {vectorMatrix.shape}'
     targetId = idList[-1]
     targetVec = one_hot_encode(targetId, vocabSize)
-    return vectorMatrix, targetId
+    return filteredMatrix, targetVec
 
 
-for i in range(0, 1000):
-    _, x = vectorize_context_seq(textIds[i:(i+101)], reverseIdx, vocabSize)
-    print(reverseIdx[x])
+def generate_train_data(textIds, batchSize, reverseIdx, vocabSize):
+    """ Generates batch for fitting """
+    textLength = len(textIds)
+    # pick random start point within the text
+    startLoc = np.random.randint(0, (textLength - SEQ_SIZE))
+    endLoc = startLoc + batchSize
+    chunkSize = SEQ_SIZE + 1
+    batchFeatures = []
+    batchTargets = []
+    for i in range(startLoc, endLoc):
+        currentIds = textIds[i : (i + chunkSize)]
+        filteredMatrix, targetVec = vectorize_context_seq(currentIds, reverseIdx, vocabSize)
+        batchFeatures.append(filteredMatrix)
+        batchTargets.append(targetVec)
+    featureArray = np.array(batchFeatures)
+    targetArray = np.array(batchTargets)
+    yield(featureArray, targetArray)
+
+textIds, wordIdx, reverseIdx, vocabSize = text_to_word_ids(TEXT_PATH)
+batchSize = 10
+
+# model
+inputs = keras.layers.Input(shape=(SEQ_SIZE, 768))
+lstm = keras.layers.LSTM(units=768, return_sequences=True)(inputs)
+dense = keras.layers.Dense(units=vocabSize, activation='softmax')(lstm)
+model = keras.models.Model(inputs=inputs, outputs=dense)
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+model.fit_generator(generate_train_data(textIds, batchSize, reverseIdx, vocabSize), steps_per_epoch=10000, epochs=10)
